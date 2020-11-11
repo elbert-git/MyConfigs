afca25391efa1ec6e5dd230b525d31265bb22105
elbert-git <elbert.nathanaeltkg@gmail.com>
2020-11-11 19:39:57 +0800

    updated configs

---
+++ overwriteConfigFiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#copy vim config file and folders
+cp ./.vimrc ~/.vimrc
+cp ./.vim ~/.vim
+
+#copy emacs config file and folders
+cp  ./.emacs ~/.emacs
+cp ./.emacs.d ~/.emacs.d
+
+#copy tmux config file
+cp ./.tmux.conf ~/.tmux.conf
+
